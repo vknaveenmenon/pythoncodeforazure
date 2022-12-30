e9145ecaa97d5df41320303fe92601c4a6099fe3
Christinaipsr <89643500+Christinaipsr@users.noreply.github.com>
2022-12-30 11:16:25 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello DEVOPS Training World!!")
+    return HttpResponse("Hello AZURE APP SERVICE Training World!!")
