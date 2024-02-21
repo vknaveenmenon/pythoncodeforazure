19359f42f91255cb689210a0224955fffb770bcf
Naveen Vadakkekurupath <107788691+vknaveenmenon@users.noreply.github.com>
2024-02-21 10:57:55 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello AZURE Training World CI/CD TESTING !!")
+    return HttpResponse("Hello AZURE World")
